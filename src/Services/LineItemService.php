8f85c86f11b7542cd4ee21ea1b04e894ab9eb1d6
tam5 <arimiller92@gmail.com>
2021-04-26 15:22:47 -0400

    make a abstract service

--- src/Services/LineItemService.php
+++ src/Services/LineItemService.php
@@ -2,54 +2,13 @@
 
 namespace LiveIntent\SDK\Services;
 
-use Illuminate\Http\Client\Factory;
-
-class LineItemService
+class LineItemService extends AbstractService
 {
-    private $path = '/strategy';
-
-    private $access_token = '';
-    private $token_type = '';
-    private $expires_in = 0;
-
     /**
      *
      */
     public function find($id)
     {
-        return $this->request('GET', "{$this->path}/{$id}");
-    }
-
-    /**
-     *
-     */
-    public function request($method, $path)
-    {
-        $factory = new Factory();
-        $client = $factory->withOptions([
-            'base_uri' => 'http://localhost:33001',
-        ]);
-
-        $client_id = 'ari';
-        $client_secret =  '93f129a60f17264feab81a260256f13e';
-
-        if (!$this->access_token) {
-            $response = $client->asForm()->post('oauth/token', [
-                'client_id' => $client_id,
-                'client_secret' => $client_secret,
-                'grant_type' => 'client_credentials',
-                'scope' => 'openid'
-            ]);
-            $this->access_token = $response->json()['access_token'];
-
-            dump($this->access_token);
-
-        }
-
-        return $client->send($method, $path, [
-            'headers' => [
-                'Authorization' => "Bearer {$this->access_token}"
-            ]
-        ]);
+        return $this->request('GET', "strategy/{$id}");
     }
 }
