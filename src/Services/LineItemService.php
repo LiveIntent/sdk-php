7b405f7b4ec4766213feee3ba20eb9db972c0c41
tam5 <arimiller92@gmail.com>
2021-04-29 12:35:08 -0400

    add more services

--- src/Services/LineItemService.php
+++ src/Services/LineItemService.php
@@ -4,9 +4,6 @@ namespace LiveIntent\Services;
 
 use LiveIntent\LineItem;
 
-// there is a reason we do this, it's for DX, so we can type hint
-// and comment all the methods. the file will be auto generated anyway
-
 /**
  * @method \LiveIntent\LineItem find($id, $opts = [])
  * @method \LiveIntent\LineItem create($attributes, $opts = [])
