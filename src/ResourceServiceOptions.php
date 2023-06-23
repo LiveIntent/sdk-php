d6500981e027fc265b69279522fad0348640dec0
Chintan Shah <cshah@liveintent.com>
2023-06-23 09:34:40 -0400

    feat(MT-4270): support for preventing calls in BaseService.php from throwing an exception when the request is a non-200 status

--- src/ResourceServiceOptions.php
+++ src/ResourceServiceOptions.php
@@ -6,8 +6,23 @@ class ResourceServiceOptions
 {
     public bool $keepRawResponse;
 
+    public bool $manuallyHandleRequestErrors;
+
+    public function __construct()
+    {
+        $this->keepRawResponse = false;
+        $this->manuallyHandleRequestErrors = false;
+    }
+
     public function withRawResponse(): void
     {
         $this->keepRawResponse = true;
     }
+
+    public function withManuallyHandledRequestErrors(): void
+    {
+        $this->manuallyHandleRequestErrors = true;
+    }
+
+
 }
